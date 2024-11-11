5c8ff23e2a2dc018bbebb551a5bc6c1f0743f88c
Denys Vorobyov <vorobyoff.denys@gmail.com>
2024-11-11 16:58:57 +0300

    Added Gaprom::DB class

--- lib/Gazprom/DB.pm
+++ lib/Gazprom/DB.pm
@@ -10,6 +10,26 @@ sub init {
     my %params = @_;
 
     $config = $params{config} or die "config argument is required";
+    $config = $config->{database} or
+        die "database section not found in supplied config data";
+}
+
+sub get_connection {
+    my $pkg = shift;
+
+    my $dsn = sprintf("dbi:Pg:dbname=%s;host=%s;port=%s",
+        $config->{db_name}
+        $config->{db_host},
+        $config->{db_port}
+    );
+    my %options = (
+        RaiseError => 1,
+        PrintError => 0,
+        AutoCommit => 0
+    );
+    my $dbh = DBI->connect($dsn, $config->{db_user}, $config->{db_password}, \%opts) or
+        die "Cannot connect to DB! Error: ".$DBI::errstr;
+    return $dbh;
 }
 
 1;
