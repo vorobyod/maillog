5b706cb59a3d9db378f4c7039027a730fc647302
Denys Vorobyov <vorobyoff.denys@gmail.com>
2024-11-12 14:51:38 +0300

    Fixed connection parameters

--- lib/Gazprom/DB.pm
+++ lib/Gazprom/DB.pm
@@ -3,22 +3,25 @@ package Gazprom::DB;
 use strict;
 use warnings;
 
-my $config = '';
+use DBI;
+
+my $_DATA = {};
 
 sub init {
     my $pkg = shift;
     my %params = @_;
 
-    $config = $params{config} or die "config argument is required";
-    $config = $config->{database} or
+    my $config = $params{config} or die "config argument is required";
+    $_DATA->{config} = $config->{database} or
         die "database section not found in supplied config data";
 }
 
 sub get_connection {
     my $pkg = shift;
+    my $config = $_DATA->{config};
 
     my $dsn = sprintf("dbi:Pg:dbname=%s;host=%s;port=%s",
-        $config->{db_name}
+        $config->{db_name},
         $config->{db_host},
         $config->{db_port}
     );
@@ -27,7 +30,7 @@ sub get_connection {
         PrintError => 0,
         AutoCommit => 0
     );
-    my $dbh = DBI->connect($dsn, $config->{db_user}, $config->{db_password}, \%opts) or
+    my $dbh = DBI->connect($dsn, $config->{db_user}, $config->{db_password}, \%options) or
         die "Cannot connect to DB! Error: ".$DBI::errstr;
     return $dbh;
 }
