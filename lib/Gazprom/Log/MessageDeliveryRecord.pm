6394d8b2bc29f4ae6b9cb44fe51f47a07d2a5349
Denys Vorobyov <vorobyoff.denys@gmail.com>
2024-11-13 13:42:46 +0300

    Implemented from_string() method for MessageDeliveryRecord class

--- lib/Gazprom/Log/MessageDeliveryRecord.pm
+++ lib/Gazprom/Log/MessageDeliveryRecord.pm
@@ -3,11 +3,32 @@ package Gazprom::Log::MessageDeliveryRecord;
 use strict;
 use warnings;
 
-use base qw( Gazprom::Log::Record );
+use base qw( Gazprom::Log::LogRecord );
+use Gazprom::Log::LogRecord;
 
 sub from_string {
     my $pkg = shift;
-    die "Not implemented";
+    my $string = shift or die "string is required";
+
+    my $self = {};
+
+    if ($string =~ /:blackhole: \</) 
+        # Special parse case - blackhole mail router{
+        my @rec = split(' ', $string, 7);
+        my ($address) = $rec[5] =~ /\<(.*)\>/;
+        $self = {
+            created_at => join(' ', @rec[0 .. 1]),
+            int_id => $rec[2],
+            str => join(' ', @rec[2 .. 6]),
+            address => $address
+        };
+    } else {
+        # Normal delivery log records
+        $self = $pkg->SUPER::from_string($string);
+    }
+
+    bless $self, $pkg;
+    return $self;
 }
 
 sub match_flag {
