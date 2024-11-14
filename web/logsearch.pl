8d4178103ec71b0f6fbd4ec84de4edef11abe9f6
Denys Vorobyov <vorobyoff.denys@gmail.com>
2024-11-14 15:17:13 +0300

    Added search action

--- web/logsearch.pl
+++ web/logsearch.pl
@@ -1,9 +1,64 @@
+#!/usr/bin/env perl
+
 use Mojolicious::Lite -signatures;
- 
+
+our %ENV;
+use lib ($ENV{LOGPARSER_HOME}.'/lib');
+
+use Gazprom::Config;
+use Gazprom::DB;
+
+use constant RESULT_ROWS_MAX => 100;
+
+my $script_home_dir = $ENV{LOGPARSER_HOME} or
+	die("LOGPARSER_HOME env variable is not set");
+my $config_file = "$script_home_dir/etc/logparser.conf";
+
+my $config = Gazprom::Config->get_config($config_file);
+Gazprom::DB->init(config => $config);
+my $dbh = Gazprom::DB->get_connection();
+
 get '/search' => sub ($c) {
-  my $address = $c->param('address');
-  $c->render(template => 'search_form');
+  my $address = $c->('address');
+  $c->render(template => 'search_form', result => []);
 };
+
+post '/search' => sub ($c) {
+    my $address = $c->param('address');
+
+    my $search_sql = "
+        SELECT created, str FROM message WHERE str ILIKE ?
+        UNION
+        SELECT created, str FROM log WHERE address = ?
+        ORDER BY int_id ASC, created ASC
+    ";
+
+    my $count_sql = "SELECT COUNT(*) FROM ($search_sql)";
+
+    # Let's count the result set
+    my $query = $count_sql; 
+    my $sth = $dbh->prepare($query) or
+        die "Cannot prepare $query !!! Resason: ".$dbh->errstr();
+    $sth->execute('%'.$address.'%', $address) or
+        die "Error executing $query !!! Reason: ". $dbh->errstr();
+    my $result = $sth->fetchrow_arrayref();
+    my $rows_num = $result->[0];
+
+    if ($rows_num > RESULT_ROWS_MAX) {
+        $c->stash(msg => 'More than '.RESULT_ROWS_MAX.
+            ' rows returned! Limiting to '.RESULT_ROWS_MAX);
+        $search_sql .= ' LIMIT '.RESULT_ROWS_MAX;
+    }
+
+    # Run the actual search query
+    $query = $search_sql;
+    $sth = $dbh->prepare($query) or
+        die "Cannot prepare $query !!! Resason: ".$dbh->errstr();
+    $sth->execute('%'.$address.'%', $address) or
+        die "Error executing $query !!! Reason: ". $dbh->errstr();
+    $result = $sth->fetchall_arrayref();
+    $c->render(template => 'search_form', result => $result);
+}
  
 app->start;
 
@@ -15,3 +70,17 @@ __DATA__
 <input type="text" id="address" name="address" required size="10" />
 <input type="submit" value="Search" />
 </form>
+<% if (scalar(@$result)) { %>
+<table>
+<tr>
+    <th>Created</th>
+    <th>Log</th>
+</tr>
+<% for my $row (@$result) { %>
+<tr>
+    <td><%= $row->[0] %></td>
+    <td><%= $row->[1] %></td>
+</tr>
+<% } %>
+</table>
+<% } %>
